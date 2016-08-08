9cc8b9cf8fdaf1bd0c78efbdbca50d342223b61d
Razvan <razvan.vasile05@gmail.com>
2016-08-08 15:10:48 +0100

    Create template class for an element

---
+++ rml/element.py
@@ -0,0 +1,7 @@
+# TODO: make element as an abstract class
+class Element:
+
+    # TODO: use **kwargs
+    def __init__(self, name, position):
+        self.element_type = name
+        self.s = position
