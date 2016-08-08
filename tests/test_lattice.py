27a0165d45f52114ebb65d59cf8e4f84f3232881
Will Rogers <wahrogers@gmail.com>
2016-08-08 15:25:45 +0100

    Test simple lattice with one element.

--- tests/test_lattice.py
+++ tests/test_lattice.py
@@ -1,9 +1,23 @@
 import rml.lattice
+import rml.element
 
 def test_create_lattice():
     l = rml.lattice.Lattice()
     assert(len(l)) == 0
 
+
 def test_non_negative_lattice():
     l = rml.lattice.Lattice()
     assert(len(l)) >= 0
+
+
+def test_lattice_with_one_element():
+    l = rml.lattice.Lattice()
+    element_length = 1.5
+    e = rml.element.Element('dummy', element_length)
+    l.append_element(e)
+    # There is one element in the lattice.
+    assert(len(l) == 1)
+    # The total length of the lattice is the same as its one element.
+    assert l.length() = element_length
+
