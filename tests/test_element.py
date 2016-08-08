ae2f1fcbe759e821291f7f22751c24c1b4e55095
Will Rogers <wahrogers@gmail.com>
2016-08-08 17:05:00 +0100

    Add tests for getting lattice elements and element families.

--- tests/test_element.py
+++ tests/test_element.py
@@ -5,3 +5,9 @@ def test_create_element():
     e = rml.element.Element('BPM', 6.0)
     assert e.element_type == 'BPM'
     assert e.length == 6.0
+
+
+def test_add_element_to_family():
+    e = rml.element.Element('dummy', 0.0)
+    e.add_to_family('fam')
+    assert 'fam' in e.families
