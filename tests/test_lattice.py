dce5cdebb46375d41b61cf19e540a5014433739e
Will Rogers <wahrogers@gmail.com>
2016-08-08 15:30:30 +0100

    A lattice object should have a name.

--- tests/test_lattice.py
+++ tests/test_lattice.py
@@ -1,9 +1,14 @@
 import rml.lattice
 import rml.element
 
+
+DUMMY_NAME = 'dummy'
+
+
 def test_create_lattice():
-    l = rml.lattice.Lattice()
+    l = rml.lattice.Lattice(DUMMY_NAME)
     assert(len(l)) == 0
+    assert l.name == DUMMY_NAME
 
 
 def test_non_negative_lattice():
@@ -12,12 +17,12 @@ def test_non_negative_lattice():
 
 
 def test_lattice_with_one_element():
-    l = rml.lattice.Lattice()
+    l = rml.lattice.Lattice(DUMMY_NAME)
     element_length = 1.5
-    e = rml.element.Element('dummy', element_length)
+    e = rml.element.Element('dummy_element', element_length)
     l.append_element(e)
     # There is one element in the lattice.
     assert(len(l) == 1)
     # The total length of the lattice is the same as its one element.
-    assert l.length() = element_length
+    assert l.length() == element_length
 
