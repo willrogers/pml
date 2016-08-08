8603e7deba3257ebf1c7a605b40f8ba3fec4a83c
Razvan <razvan.vasile05@gmail.com>
2016-08-08 17:21:18 +0100

    Return all elements of the lattice

--- rml/lattice.py
+++ rml/lattice.py
@@ -21,3 +21,9 @@ class Lattice:
         for e in self._elements:
             total_length += e.length
         return total_length
+
+
+    # TODO return elements for a specific family name
+    def get_elements(self, family='*'):
+        ''' Get all the elements of a lattice '''
+        return self._elements
