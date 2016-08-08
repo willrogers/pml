f6b7f227354586a660a02868ba4cc93ae6e5aad3
Razvan <razvan.vasile05@gmail.com>
2016-08-08 17:19:54 +0100

    Create add_to_family method

--- rml/element.py
+++ rml/element.py
@@ -1,7 +1,11 @@
 # TODO: make element as an abstract class
 class Element:
-
     # TODO: use **kwargs
     def __init__(self, name, length):
         self.element_type = name
         self.length = length
+        self.families = []
+
+
+    def add_to_family(self, family):
+        self.families.append(family)
