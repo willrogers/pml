e366f6da5673a4c92ffcf65492951e0c6fc886ed
Razvan <razvan.vasile05@gmail.com>
2016-08-10 14:36:53 +0100

    Test before creating the get_pv() method

--- tests/test_element.py
+++ tests/test_element.py
@@ -1,3 +1,6 @@
+import pkg_resources
+pkg_resources.require('cothread')
+import cothread
 import rml.element
 
 
@@ -11,3 +14,10 @@ def test_add_element_to_family():
     e = rml.element.Element('dummy', 0.0)
     e.add_to_family('fam')
     assert 'fam' in e.get_families()
+
+
+def test_get_pv_value():
+    PV = 'SR22C-DI-EBPM-04:SA:X'
+    e = rml.element.Element('dummy', 0.0, pv=PV)
+    result = e.get_pv('x')
+    assert isinstance(result, float)
