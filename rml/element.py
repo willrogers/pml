3146b659f216a0a32482e6ce304d61160b798f30
Razvan <razvan.vasile05@gmail.com>
2016-08-10 14:46:13 +0100

    Create the get_pv() method

--- rml/element.py
+++ rml/element.py
@@ -2,18 +2,36 @@
 @param element_type: type of the element
 @param length: length of the element
 '''
-
+import pkg_resources
+pkg_resources.require('cothread')
+from cothread.catools import caget
 
 class Element(object):
 
-    def __init__(self, elem_type, length):
-        # TODO: use **kwargs
-        self.element_type = elem_type
+    def __init__(self, element_type, length, **kwargs):
+        self.element_type = element_type
         self.length = length
         self.families = set()
 
+        # Getting the pv value
+        self.pv = kwargs.get('pv', None)
+        self._field = {'x', 'y'}
+
     def add_to_family(self, family):
         self.families.add(family)
+
+    def get_pv(self, field, handle='readback'):
+        """
+        Get pv value for the given field.
+        Currently only supports readback handle
+        """
+
+        if not field in self._field:
+            raise RuntimeError("Field {0} doesn't exist.".format(field))
+        elif handle == 'readback':
+            return caget(self.pv)
+        else:
+            raise ValueError("Unknown handle {0}".format(handle))
 
     def get_type(self):
         return self.element_type
