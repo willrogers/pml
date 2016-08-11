9cfdcca921290c4c231553a254c116b847971a6f
Razvan <razvan.vasile05@gmail.com>
2016-08-11 15:58:29 +0100

    Cleaning up

--- tests/test_element.py
+++ tests/test_element.py
@@ -9,17 +9,17 @@ pkg_resources.require('cothread')
 def get_elements(fields):
     # Return a list of requested elements
     # <list> fields: 'x' or 'y'
-    result = list()
+    result = dict()
     if 'x' in fields:
         pv_x = 'SR22C-DI-EBPM-04:SA:X'
         e = rml.element.Element('dummy_x', 0.0, cs='epics')
         e.set_pv('x', pv_x)
-        result.append(e)
+        result['x'] = e
     if 'y' in fields:
         pv_y = 'SR22C-DI-EBPM-04:SA:Y'
         e = rml.element.Element('dummy_y', 0.0, cs='epics')
         e.set_pv('y', pv_y)
-        result.append(e)
+        result['y'] = e
     return result
 
 
@@ -36,14 +36,14 @@ def test_add_element_to_family():
 
 
 def test_get_pv_value():
-    pv = get_elements(['x', 'y'])
-    assert isinstance(pv[0].get_pv('x'), float)
-    assert isinstance(pv[1].get_pv('y'), float)
+    pvs = get_elements(['x', 'y'])
+    assert isinstance(pvs['x'].get_pv('x'), float)
+    assert isinstance(pvs['y'].get_pv('y'), float)
 
 
 def test_get_pv_exceptions():
     pvs = get_elements(['x', 'y'])
     with pytest.raises(PvUnknownFieldError):
-        pvs[0].get_pv('unknown_field')
+        pvs['x'].get_pv('unknown_field')
     with pytest.raises(PvUnknownHandleError):
-        pvs[1].get_pv('y', 'unkown_handle')
+        pvs['y'].get_pv('y', 'unkown_handle')
