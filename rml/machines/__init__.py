4d3b49d7aa11aa4fef0a9b928640ba05264c4246
Razvan Vasile <razvan.vasile07@gmail.com>
2016-08-14 19:37:42 +0100

    Implement method to get pv names for a specific mode

---
+++ rml/machines/__init__.py
@@ -0,0 +1,17 @@
+import os
+import sqlite3
+
+def load_pvs(machine):
+    basepath = os.path.dirname(__file__)
+    filepath = os.path.abspath(os.path.join(basepath, machine, 'data.sqlite'))
+
+    pv_names = []
+    conn = sqlite3.connect(filepath)
+    c = conn.cursor()
+
+    c.execute('SELECT pv FROM pvs')
+    while 1:
+        row = c.fetchone()
+        if not row: break
+        pv_names.append(row)
+    return pv_names
