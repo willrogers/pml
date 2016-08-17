9d041287e5e0d1950d5dcda23f6f68522d287282
Razvan <razvan.vasile07@gmail.com>
2016-08-17 16:47:36 +0100

    Test if pvs are loaded correctly from the database

--- tests/test_machine.py
+++ tests/test_machine.py
@@ -2,7 +2,8 @@ import rml.machines
 
 
 def test_machine_load_elements():
-    lattice = rml.machines.get_elements(machine='SRI21', elemType='BPM')
+    lattice = rml.machines.get_elements(machine='SRI21', elem_type='BPM')
     assert len(lattice) == 173
     for element in lattice.get_elements():
-        assert element.get_pv_name('readback')
+        assert isinstance(element.get_pv_name('readback', 'x'), str)
+        assert isinstance(element.get_pv_name('readback', 'y'), str)
