20ceff5240f8619c468883744549380899f5ee20
Razvan <razvan.vasile07@gmail.com>
2016-08-17 12:01:23 +0100

    Update tests to handle setpoint and readback handles

--- tests/test_element.py
+++ tests/test_element.py
@@ -7,7 +7,7 @@ pkg_resources.require('cothread')
 
 
 @pytest.fixture
-def get_elements(fields):
+def get_elements(handle, fields):
     # Return a list of requested elements
     # <list> fields: 'x' or 'y'
     dummy_control_system = dummycontrolsystem.DummyControlSystem()
@@ -15,12 +15,12 @@ def get_elements(fields):
     if 'x' in fields:
         pv_x = 'SR22C-DI-EBPM-04:SA:X'
         e = rml.element.Element('dummy_x', 0.0, cs=dummy_control_system)
-        e.put_pv_name('x', pv_x)
+        e.put_pv_name(handle, 'x', pv_x)
         result['x'] = e
     if 'y' in fields:
         pv_y = 'SR22C-DI-EBPM-04:SA:Y'
         e = rml.element.Element('dummy_y', 0.0, cs=dummy_control_system)
-        e.put_pv_name('y', pv_y)
+        e.put_pv_name(handle, 'y', pv_y)
         result['y'] = e
     return result
 
@@ -37,18 +37,22 @@ def test_add_element_to_family():
     assert 'fam' in e.families
 
 
-def test_get_set_pv_value():
+def test_readback_pvs():
     # Tests to get/set pv names and/or values
-    pvs = get_elements(['x', 'y'])
-    assert isinstance(pvs['x'].get_pv_value('x'), float)
-    assert isinstance(pvs['y'].get_pv_value('y'), float)
+    pvs = get_elements('readback', ['x', 'y'])
+    assert isinstance(pvs['x'].get_pv_value('readback', 'x'), float)
+    assert isinstance(pvs['y'].get_pv_value('readback', 'y'), float)
+
+
+def test_setpoint_pvs():
+    pvs = get_elements('setpoint', ['x'])
     pvs['x'].put_pv_value('x', 4)
-    assert pvs['x'].get_pv_value('x') == 4
+    assert pvs['x'].get_pv_value('setpoint', 'x') == 4
 
 
 def test_get_pv_exceptions():
-    pvs = get_elements(['x', 'y'])
+    pvs = get_elements('setpoint', ['x', 'y'])
     with pytest.raises(PvUnknownFieldError):
-        pvs['x'].get_pv_value('unknown_field')
+        pvs['x'].get_pv_value('setpoint', 'unknown_field')
     with pytest.raises(PvUnknownHandleError):
-        pvs['y'].get_pv_value('y', 'unkown_handle')
+        pvs['y'].get_pv_value('unkown_handle', 'y')
--- tests/test_machine.py
+++ tests/test_machine.py
@@ -2,5 +2,5 @@ import rml.machines
 
 
 def test_machine_load_elements():
-    pv_names = rml.machines.get_elements(machine='SRI21', elemType='BPM')
-    assert pv_names
+    lattice = rml.machines.get_elements(machine='SRI21', elemType='BPM')
+    assert len(lattice) == 173
