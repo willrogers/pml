7b2456218fc2acd49f61f57591ccedef2de9fcec
Razvan <razvan.vasile07@gmail.com>
2016-08-17 10:06:59 +0100

    Change tests with more suitable variable names for elements

--- tests/test_element.py
+++ tests/test_element.py
@@ -26,15 +26,15 @@ def get_elements(fields):
 
 
 def test_create_element():
-    e = rml.element.Element(element_name=0, element_type='BPM', length=6.0)
-    assert e.get_type() == 'BPM'
-    assert e.get_length() == 6.0
+    e = rml.element.Element(4, 'BPM', length=6.0)
+    assert 'BPM' in e.families
+    assert e.length == 6.0
 
 
 def test_add_element_to_family():
     e = rml.element.Element('dummy', 0.0)
     e.add_to_family('fam')
-    assert 'fam' in e.get_families()
+    assert 'fam' in e.families
 
 
 def test_get_set_pv_value():
--- tests/test_lattice.py
+++ tests/test_lattice.py
@@ -36,7 +36,7 @@ def test_lattice_with_one_element(simple_element_and_lattice):
     # There is one element in the lattice.
     assert(len(lattice) == 1)
     # The total length of the lattice is the same as its one element.
-    assert lattice.get_length() == element.get_length()
+    assert lattice.get_length() == element.length
     # Get all elements
     assert lattice.get_elements() == [element]
 
