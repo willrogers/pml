abefbbc99e7e62bed31db549519807feee7254f9
Razvan <razvan.vasile07@gmail.com>
2016-08-17 13:22:33 +0100

    Test to get different pv names for an element

--- tests/test_machine.py
+++ tests/test_machine.py
@@ -4,3 +4,5 @@ import rml.machines
 def test_machine_load_elements():
     lattice = rml.machines.get_elements(machine='SRI21', elemType='BPM')
     assert len(lattice) == 173
+    for element in lattice.get_elements():
+        assert element.get_pv_name('readback')
