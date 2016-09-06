3a967dc51ec7c04aa626a662d99a40091d747ce3
Razvan <razvan.vasile07@gmail.com>
2016-09-06 13:36:01 +0100

    Tests for adding multiple devices on the same element

--- tests/test_element.py
+++ tests/test_element.py
@@ -1,38 +1,25 @@
-import pkg_resources
 from rml.exceptions import PvException
 import rml.element
+import rml.device
 import cs_dummy
 from rml.units import UcPoly
 import pytest
 
 
 @pytest.fixture
-def get_elements(handle, fields, unit_conversion=UcPoly([1, 0])):
-    # Return a list of requested elements
-    # <list> fields: 'x' or 'y'
-    dummy_control_system = cs_dummy.DummyControlSystem()
-#    dummy_control_system.get = mock.MagicMock(return_value=4)
-#    self._cs.get('duumy')
-#    dummy_control_system.get.assert_called_with('dummy')
-#    dummy_control_system.set.assert_called_with(pv, value)
-    result = dict()
-    if 'x' in fields:
-        pv_x = 'SR22C-DI-EBPM-04:SA:X'
-        e = rml.element.Element('dummy_x', length=0.0, cs=dummy_control_system,
-                                uc=unit_conversion)
-        e.put_pv_name(handle, 'x', pv_x)
-        if handle == 'setpoint':
-            e.put_pv_value('x', 0.0, unit='machine')
-        result['x'] = e
-    if 'y' in fields:
-        pv_y = 'SR22C-DI-EBPM-04:SA:Y'
-        e = rml.element.Element('dummy_y', length=0.0, cs=dummy_control_system,
-                                uc=unit_conversion)
-        e.put_pv_name(handle, 'y', pv_y)
-        if handle == 'setpoint':
-            e.put_pv_value('y', 0.0, unit='machine')
-        result['y'] = e
-    return result
+def get_elements(length=0.0, uc=UcPoly([0, 1])):
+    cs = cs_dummy.DummyControlSystem()
+
+    element = rml.element.Element(1, length=length, cs=cs_dummy, uc=uc)
+    rb_pv = 'SR22C-DI-EBPM-04:SA:X'
+    sp_pv = 'SR22C-DI-EBPM-04:SA:Y'
+    device1 = rml.device.Device(rb_pv, sp_pv, cs, uc)
+    device2 = rml.device.Device(sp_pv, rb_pv, cs, uc)
+
+    element.add_device('x', device1)
+    element.add_device('y', device2)
+
+    return element
 
 
 def test_create_element():
@@ -47,53 +34,55 @@ def test_add_element_to_family():
     e.add_to_family('fam')
     assert 'fam' in e.families
 
-
+'''
 def test_readback_pvs():
     # Tests to get/set pv names and/or values
-    pvs = get_elements('readback', ['x', 'y'])
-    assert isinstance(pvs['x'].get_pv_value('readback', 'x'), float)
-    assert isinstance(pvs['y'].get_pv_value('readback', 'y'), float)
-    assert isinstance(pvs['x'].get_pv_name('readback'), dict)
-    assert isinstance(pvs['y'].get_pv_name('readback', 'y'), str)
+    element = get_elements()
+    element.put_pv_value('x', 40)
+    element.put_pv_value('y', 40)
+    print element.get_pv_value('x', 'readback')
+    assert isinstance(element.get_pv_value('x', 'readback'), float)
+    assert isinstance(element.get_pv_value('y', 'readback'), float)
+    assert isinstance(element.get_pv_name('x'), dict)
+    assert isinstance(element.get_pv_name('y', 'readback'), str)
+'''
 
 
 def test_setpoint_pvs():
-    pvs = get_elements('setpoint', ['x', 'y'])
-    assert isinstance(pvs['x'].get_pv_value('setpoint', 'x'), float)
-    assert isinstance(pvs['y'].get_pv_value('setpoint', 'y'), float)
-    assert isinstance(pvs['x'].get_pv_name('setpoint'), dict)
-    assert isinstance(pvs['y'].get_pv_name('setpoint', 'y'), str)
-
-    pvs['x'].put_pv_value('x', 4)
-    assert pvs['x'].get_pv_value('setpoint', 'x') == 4
+    element = get_elements()
+    element.put_pv_value('x', 40.0)
+    element.put_pv_value('y', 40.0)
+    assert isinstance(element.get_pv_value('x', 'setpoint'), float)
+    assert isinstance(element.get_pv_value('y', 'setpoint'), float)
+    assert isinstance(element.get_pv_name('x'), list)
+    assert isinstance(element.get_pv_name('y', 'setpoint'), str)
 
 
 def test_get_pv_exceptions():
-    pvs = get_elements('setpoint', ['x', 'y'])
+    element = get_elements()
     with pytest.raises(PvException):
-        pvs['x'].get_pv_value('setpoint', 'unknown_field')
+        element.get_pv_value('setpoint', 'unknown_field')
     with pytest.raises(PvException):
-        pvs['y'].get_pv_value('unknown_handle', 'y')
+        element.get_pv_value('unknown_handle', 'y')
 
     with pytest.raises(PvException):
-        pvs['x'].get_pv_name('unknown_handle')
-
-
-def test_put_pv_exceptions():
-    pvs = get_elements('setpoint', ['x', 'y'])
-    with pytest.raises(PvException):
-        pvs['x'].put_pv_value('unknown_field', 4.0)
-    with pytest.raises(PvException):
-        pvs['y'].put_pv_name('unknown_handle', 'x',
-                             'SR21C-DI-EBPM-04:SA:Y')
+        element.get_pv_name('unknown_handle')
 
 
 def test_identity_conversion():
-    identity_conversion = UcPoly([1, 0])
-    pvs = get_elements('setpoint', ['x'], unit_conversion=identity_conversion)
-    pvs['x'].put_pv_value('x', 4.0, unit='machine')
-    pvs['x'].put_pv_value('x', 4.0, unit='physics')
-    value_physics = pvs['x'].get_pv_value('setpoint', 'x', 'physics')
-    value_machine = pvs['x'].get_pv_value('setpoint', 'x', 'machine')
-    assert value_physics == 4.0
+    uc_id = UcPoly([1, 0])
+    element = get_elements(uc=uc_id)
+    element.put_pv_value('x', 4.0, unit='machine')
+    value_physics = element.get_pv_value('x', 'setpoint', 'physics')
+    element.put_pv_value('x', 4.0, unit='physics')
+    value_machine = element.get_pv_value('x', 'setpoint', 'machine')
     assert value_machine == 4.0
+    assert value_physics == 4.0
+
+
+'''
+#    dummy_control_system.get = mock.MagicMock(return_value=4)
+#    self._cs.get('duumy')
+#    dummy_control_system.get.assert_called_with('dummy')
+#    dummy_control_system.set.assert_called_with(pv, value)
+'''
