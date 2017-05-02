fb1ca1faeeb298d70eba4e47ea223e6bf9f41273
Razvan <razvan.vasile07@gmail.com>
2017-05-02 10:20:45 +0100

    Remove a variable causing a test to fail (bugged)

--- test/test_element.py
+++ test/test_element.py
@@ -58,7 +58,7 @@ def test_get_pv_name(pv_type, test_element):
     assert isinstance(test_element.get_pv_name('y', pv_type), str)
 
 
-def test_put_pv_value(pv_type, test_element):
+def test_put_pv_value(test_element):
     test_element.put_pv_value('x', 40.3)
     test_element.get_device('x')._cs.put.assert_called_with('SR22C-DI-EBPM-04:SA:Y', 40.3)
 
