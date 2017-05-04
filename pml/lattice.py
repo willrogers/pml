ee28a9f35f9a47ea79fbbc86eba5d3587c973cff
Razvan <razvan.vasile07@gmail.com>
2017-05-04 15:17:53 +0100

    Add documentation for the lattice class

--- pml/lattice.py
+++ pml/lattice.py
@@ -1,46 +1,79 @@
+import pml
 from pml.exceptions import PvException
 from pml.exceptions import ElementNotFoundException
 
 class Lattice(object):
-    ''' Simple lattice object to hold the elements of the ring. '''
-
     def __init__(self, name, control_system):
+        """Representation of a lattice.
+
+        Represents a lattice object that contains all elements
+        of the ring. It has a name and a control system to be used
+        for unit conversion.
+
+        Args:
+            name (string): The name of the lattice.
+            control_system (ControlSystem): The control system used
+                to store the values on a pv.
+        """
         self.name = name
         self._lattice = []
         self._cs = control_system
 
     def __getitem__(self, n):
-        '''
-        Get the nth element element of the lattice.
-        '''
+        """Get the nth element of the lattice.
+
+        Args:
+            n (int): An index that represents the nth + 1 element of the
+                lattice.
+
+        Returns:
+            Element: The element asked by the index n.
+        """
+
         return self._lattice[n]
 
     def __len__(self):
-        '''
-        Get the number of elements in the lattice.
-        '''
+        """The number of elements in the lattice.
+
+        When using the len function returns the number of elements in
+        the lattice.
+
+        Returns:
+            int: The number of elements in the lattice.
+
+        """
         return len(self._lattice)
 
     def get_length(self):
-        '''
-        Get the length of the lattice in meters.
-        '''
+        """Returns the length of the lattice.
+
+        Returns:
+            float: The length of the lattice.
+        """
         total_length = 0
         for e in self._lattice:
             total_length += e.get_length()
         return total_length
 
     def add_element(self, element):
-        '''
-        Add an element to the lattice.
-        '''
+        """Add an element to the lattice.
+
+        Args:
+            element (Element): The element to be inserted into the lattice.
+        """
         self._lattice.append(element)
 
     def get_elements(self, family=None):
-        '''
-        Get all elements of a lattice from a specified family.
-        If no family is specified, return all elements.
-        '''
+        """Get the elements of a family from the lattice.
+
+        If no family is specified it returns the whole lattice.
+
+        Args:
+            family (string): A specific family to return the elements of.
+
+        Returns:
+            list(Element): A list that contains all elements of the specified family.
+        """
         if family is None:
             return self._lattice
 
@@ -51,9 +84,11 @@ class Lattice(object):
         return matched_lattice
 
     def get_all_families(self):
-        '''
-        Get all available families inside the lattice.
-        '''
+        """Get all available families of the lattice.
+
+        Returns:
+            set(string): Contains all available families in the lattice.
+        """
         families = set()
         for element in self._lattice:
             for family in element.families:
@@ -62,9 +97,16 @@ class Lattice(object):
         return families
 
     def get_family_pvs(self, family, field, handle):
-        '''
-        Get all pvs from a specified family, field and handle.
-        '''
+        """Get all pv names for a specific family, field and handle.
+
+        Args:
+            family (string): A specific family to requests elements of.
+            field (string): The field to uniquely identify a device.
+            handle (string): It is used to identify a readback or setpoint pv.
+
+        Returns:
+            list(string): A list of readback or setpoint pvs from the device.
+        """
         elements = self.get_elements(family)
         pv_names = []
         for element in elements:
@@ -72,16 +114,36 @@ class Lattice(object):
         return pv_names
 
     def get_family_values(self, family, field, handle='setpoint'):
-        '''
-        Get all pv values from a specified family, field and handle.
-        '''
+        """Get all pv values for a set of pvs.
+
+        Args:
+            family(string): A specific family to requests the values of.
+            field(string): The field to uniquely identify a device.
+            handle(string): It is used to identify a readback or setpoint pv.
+
+        Returns:
+            list(float): A list of readback or setpoint pv values from the device.
+        """
         pv_names = self.get_family_pvs(family, field, handle)
         return self._cs.get(pv_names)
 
     def set_family_values(self, family, field, values):
-        '''
+        """Set the pv value of a given family of pvs.
+
+        The pvs are determined by family and device. Note that only setpoint
+        pvs ca be modified.
+
+        Args:
+            family(string): A specific family to set the value of.
+            field(string): The field to uniquely identify a device.
+            values(list(float)): A list of values to assign to the pvs.
+
+        Raises:
+            PvException: An exception raised in case the given list of values
+            doesn't match the number of found pvs.
+
+        """
         Given a family set the values of the pvs from a specified field.
-        '''
         # Get the number of elements in the family
         pv_names = self.get_family_pvs(family, field, 'setpoint')
         if len(pv_names) != len(values):
@@ -90,9 +152,20 @@ class Lattice(object):
         self._cs.put(pv_names, values)
 
     def get_s(self, given_element):
-        '''
-        Return the position of a given element in the lattice.
-        '''
+        """Find the position of a given element in the lattice.
+
+        Note that the given element must exist in the lattice.
+
+        Args:
+            given_element: The element that the position is being asked for.
+
+        Returns:
+            float: the position of the given element.
+
+        Raises
+            ElementNotFoundException: An exception is raised in case the element
+            doesn't exist inside the lattice.
+        """
         s_pos = 0
         for element in self._lattice:
             if element is not given_element:
@@ -102,9 +175,15 @@ class Lattice(object):
         raise ElementNotFoundException('Given element does not exist in the lattice')
 
     def get_family_s(self, family):
-        '''
-        Return a list of positions for a given family.
-        '''
+        """Get the positions for a set of elements from the same family.
+
+        Args:
+            family(string): The family the positions are being asked for.
+
+        Returns:
+            list(float): A list of floating point numbers that represent the positions
+            for each element.
+        """
         elements = self.get_elements(family)
         s_positions = list()
         for element in elements:
