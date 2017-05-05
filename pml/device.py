8211889ed344e163a5ec056c6084b1022a4f456b
Razvan <razvan.vasile07@gmail.com>
2017-05-05 10:21:46 +0100

    Add documentation for the device class

--- pml/device.py
+++ pml/device.py
@@ -3,12 +3,20 @@ import pml
 
 
 class Device(object):
-    ''' Template module that represents a device. '''
-    # should be a length parameter here
     def __init__(self, cs, rb_pv=None, sp_pv=None):
-        '''
-        Create a device with a control system and readback/setpoint pvs.
-        '''
+        """A device attached on an element.
+
+        Contains a control system, readback and setpoint pvs. A readback
+        or setpoint pv is required when creating a device otherwise a
+        PvException is raised.
+
+        Args:
+            cs (ControlSystem): Control system object used to get and set
+                the value of a pv.
+            rb_pv (string): A readback pv. This value cannot be changed.
+            sp_pv (string): A setpoint pv. This value can be read and also
+                changed.
+        """
         self.rb_pv = rb_pv
         self.sp_pv = sp_pv
         self._cs = cs
@@ -21,10 +29,16 @@ class Device(object):
             raise PvException("Readback or setpoint pvs need to be given")
 
     def put_value(self, value):
-        '''
-        Set the pv value on a setpoint pv. If the setpoint pv doesn't exist raise
-        a PvException.
-        '''
+        """Set the value of a pv.
+
+        If not setpoint pv exists a PvException is raised.
+
+        Args:
+            value(Number): The value to set on the pv.
+
+        Raises:
+            PvException: An exception occured when no setpoint pv exists.
+        """
         # Not sure if this method will need a handle flag to set
         # an initial value for readback pvs. Suppose not:
         if self.sp_pv is not None:
@@ -34,9 +48,20 @@ class Device(object):
                               .format(self.name))
 
     def get_value(self, handle):
-        '''
-        Get either the setpoint or readback pv valuue off a device.
-        '''
+        """Read the value of a readback or setpoint pv.
+
+        If neither readback or setpoint pvs exist then a PvException is raised.
+
+        Args:
+            handle(string): Handle used to get the value off a readback or setpoint
+                pv.
+
+        Returns:
+            Number: The value off the pv.
+
+        Raises:
+            PvException: In case the requested pv doesn't exist.
+        """
         if handle == pml.RB and self.rb_pv:
             return self._cs.get(self.rb_pv)
         elif handle == pml.SP and self.sp_pv:
@@ -46,9 +71,16 @@ class Device(object):
                           .format(self.name, handle))
 
     def get_pv_name(self, handle='*'):
-        '''
-        Get a pv name given a handle. If no handle is given return both pvs.
-        '''
+        """Get a pv name on a specified handle.
+
+        If no handle is specified, then both pvs are returned.
+
+        Args:
+            handle(string): The readback or setpoint handle to be returned.
+
+        Returns:
+            string: A readback or setpoint pv.
+        """
         if handle == '*':
             return [self.rb_pv, self.sp_pv]
         elif handle == pml.RB:
